a72592bf81287b25ab9c5e469cf701336e7667f6
miaozhirui <miaozhirui@ziseyiliao.com>
2016-05-24 12:12:55 +0800

    add index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function(req, res){
-
-    res.end('hello world');
+    
+    fs.readFile('index.html', function(err,data){
+        if(err){
+            console.log(err)
+        } else {
+            console.log(data)
+            res.end(data);
+        }
+    })
 
 }).listen(3000);
 
